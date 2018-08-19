eb356a68c6fc9a53402d9226d4cfa09334b997a7
Elemar Jr <elemarjr@gmail.com>
2018-08-19 18:58:46 -0300

    minor changes

--- src/TheFlow/ProcessManager.cs
+++ src/TheFlow/ProcessManager.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.Extensions.DependencyInjection;
 using TheFlow.CoreConcepts;
 using TheFlow.Infrastructure.Stores;
 
