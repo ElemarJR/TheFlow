e9392974cbf7f02289bdd3157f4bee17d36fb357
Elemar Jr <elemarjr@gmail.com>
2018-09-18 14:55:25 -0300

    Updating versions automatically

--- Build.ps1
+++ Build.ps1
@@ -16,7 +16,7 @@ function Exec
         [Parameter(Position=0,Mandatory=1)][scriptblock]$cmd,
         [Parameter(Position=1,Mandatory=0)][string]$errorMessage = ($msgs.error_bad_command -f $cmd)
     )
-    & $cmd
+	& $cmd
 
     if ($lastexitcode -ne 0) {
         throw ("Exec: " + $errorMessage)
